e69b4ead69f2a4ce2eecc1ceffe31cec7dc70b90
Dra Das <113905340+DragyDasy@users.noreply.github.com>
2022-10-26 12:23:13 +0300

    Meniu Setari

---
+++ Marcaj/Marcaj/Models/CustomModels/MeseOptionsModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marcaj.Models.CustomModels
+{
+    public class MeseOptionsModel
+    {
+        public string Text { get; set; }
+    }
+}
--- Marcaj/Marcaj/Pages/Settings/SettingsHomePage.xaml.cs
+++ Marcaj/Marcaj/Pages/Settings/SettingsHomePage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Marcaj.Pages.Settings.Mese;
+﻿using Marcaj.Models.CustomModels;
+using Marcaj.Pages.Settings.Mese;
+using Marcaj.Pages.Tables;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,13 +17,37 @@ namespace Marcaj.Pages.Settings
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsHomePage : ContentPage
     {
+        public ObservableCollection<MeseOptionsModel> meseOpt;
         public SettingsHomePage()
         {
             InitializeComponent();
+            meseOpt = new ObservableCollection<MeseOptionsModel>
+            {
+            new MeseOptionsModel { Text="Mese Active/Inactive"},
+            new MeseOptionsModel { Text="Grupuri" }
+        };
+            optionsListColl.ItemsSource = meseOpt;
         }
         private async void btnMeseConf_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new MeseSetariMainPage());
+            optionsListColl.ItemsSource = meseOpt;
+        }
+
+        
+        private async void optionsListColl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            
+            string current = (e.CurrentSelection.FirstOrDefault() as MeseOptionsModel)?.Text;
+                   if (current == "Mese Active/Inactive")
+                    {
+                        await Navigation.PushAsync(new ConfigurareMesePage());
+                    }
+                    else if(current == "Grupuri")
+                    {
+                        await Navigation.PushAsync(new ConfigurareGrupuriPage());
+                    }
+             
         }
     }
 }
